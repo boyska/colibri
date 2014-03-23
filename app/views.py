bb234ee3751c4dda81a17517aac413bc86267a62
boyska <piuttosto@logorroici.org>
2014-03-23 15:50:28 +0100

    Better work/physical/author views

--- app/views.py
+++ app/views.py
@@ -45,9 +45,18 @@ def opera(id):
     return render_template('opera.html', opera=op)
 
 
+@app.route('/author/<id>')
+def author(id):
+    aut = models.Author.query.get_or_404(id)
+    #html = '<pre>%s</pre>%s' % (escape(str(op)),
+    #                            ','.join(_link_esemplare(es)
+    #                                     for es in op.esemplari))
+    return render_template('author.html', author=aut)
+
+
 @app.route('/search')
 def search():
-    howmany = 10
+    howmany = 50
     querystr = request.args.get('q', '')
     results = utils.search(querystr).limit(howmany)
     return render_template('esemplari.html',
